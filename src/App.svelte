466b6796110759fd5c07ca32d08caf1ee3c2f959
Shun Inoue <shun.inoue11@gmail.com>
2024-01-14 13:26:30 +0900

    add: static html

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,34 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  // import { multipleCounter } from '../stores/MultipleCounterStore'
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<div class="text-center">
+  <h1 class="text-[64px] leading-normal">Multiple Counter</h1>
+  <div
+    class="flex justify-between items-center w-96 m-auto mb-2 py-1 pl-4 rounded bg-gray-100 shadow-lg"
+  >
+    <input type="text" class="w-32 px-1 text-gray-600" />
+    <span class="text-lg font-bold">0</span>
+    <div class="flex">
+      <button type="button" class="btn bg-red-500cd rounded-l">+</button>
+      <button type="button" class="btn bg-blue-500">-</button>
+      <button type="button" class="btn bg-yellow-500 rounded-r">0</button>
+      <span class="flex justify-between items-center text-gray-500 cursor-pointer px-4">x</span>
+    </div>
   </div>
-  <h1 class="text-6xl">Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer"
-      >SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
-</main>
+  <button type="button" class="w-96 bg-green-400 rounded text-white">new counter</button>
+  <p>title list: news, new, new, new, new, new, new, new, new, new</p>
+  <p>sum of count: 0</p>
+</div>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  .btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 34px;
+    height: 35px;
+    text-align: center;
+    color: white;
   }
 </style>
